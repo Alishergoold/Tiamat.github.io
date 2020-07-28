49a6f6e0c60e28b6bd434befc71549c702e88f43
Alisher <alishergoold1989@gmail.com>
2020-07-28 20:19:17 +0500

    Add
    Add project

---
+++ js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+	$('.sliders').slick({
+		arrows: true,
+		dots: false,
+		adaptiveHeight:true,
+		slidesToShow:5,
+		slidesToScroll:1,
+		speed:500,
+		easing: 'ease',
+		infinite: true,
+		initialSlide: 0,
+		autoplay: false,
+		autoplaySpeed: 500,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: false, // swipemishki
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		waitForAnimate: false, //perelistaniye kartinok
+		centerMode:false,
+		variableWidth: false,
+		
+	});
+	// $('.sliderbig').slick({
+ // 		 arrows:false,
+ // 		 fade:true,
+	// 	 asNavFor:".slider"
+
+	// });
+	// $('.slider').slick({
+	// 	arrows:true;
+	// });
+});
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: true,
+		dots: false,
+		adaptiveHeight:true,
+		slidesToShow:3,
+		slidesToScroll:1,
+		speed:500,
+		easing: 'ease',
+		infinite: true,
+		initialSlide: 0,
+		autoplay: false,
+		autoplaySpeed: 500,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: false, // swipemishki
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		waitForAnimate: false, //perelistaniye kartinok
+		centerMode:false,
+		variableWidth: false,
+		
+	});
+});
+
+$(document).ready(function(){
+	$('.sovet__row').slick({
+		arrows: true,
+		dots: false,
+		adaptiveHeight:true,
+		slidesToShow:3,
+		slidesToScroll:1,
+		speed:500,
+		easing: 'ease',
+		infinite: true,
+		initialSlide: 0,
+		autoplay: false,
+		autoplaySpeed: 500,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: false, // swipemishki
+		swipe: true,
+		touchThreshold: 10,
+		touchMove: true,
+		waitForAnimate: false, //perelistaniye kartinok
+		centerMode:false,
+		variableWidth: false,
+	});
+});
